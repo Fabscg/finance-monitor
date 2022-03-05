7255df5d25f3ca28ab8fb14a0b0102e8f0974a14
Fabulosa <fabiscg79@gmail.com>
2022-03-04 20:28:38 -0500

    finished the service worker file

--- service-worker.js
+++ service-worker.js
@@ -5,11 +5,27 @@ const CACHE_NAME = APP_PREFIX + VERSION;
 const FILES_TO_CACHE = [
     "./index.html",
     "./public/css/style.css",
-    // "./dist/app.bundle.js",
-    // "./dist/events.bundle.js",
-    // "./dist/tickets.bundle.js",
-    // "./dist/schedule.bundle.js"
+    "./public.index.js",
+    "./routes.api.js"
 ];
+
+//Respond with cached resources
+
+self.addEventListener('fetch', function (e) {
+    e.respondingWith(
+        caches.match(e.request).then(function (request) {
+            if (request) {
+                console.log('responding with cache : ' + e.request.url)
+                return request
+            } else {
+                console.log('file is not cached, fetching :' + e.request.url)
+                return fetch(e.request)
+            }
+        })
+    )
+})
+
+//cache resources
 
 self.addEventListener('install', function (e) {
     e.waitUntil(
@@ -18,6 +34,24 @@ self.addEventListener('install', function (e) {
             return cache.addAll(FILES_TO_CACHE)
         })
     )
+})
 
 
+//delete outdated caches
+self.addEventListener('activate', function (e) {
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            let cacheKeeplist = keyList.filter(function (key) {
+                return key.indexOf(APP_PREFIX);
+            })
+            cacheKeeplist.push(CACHE_NAME)
+
+            return Promise.all(keyList).map(function (key, i) {
+                if (cacheKeeplist.indexOf(key) === -1) {
+                    console.log('deleting cache : ' + keyList[i])
+                    return caches.delete(keyList[i])
+                }
+            })
+        })
+    )
 })
