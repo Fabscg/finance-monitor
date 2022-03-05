d63db1bff4ee0561397cab22dbd8d9155a015746
Fabulosa <fabiscg79@gmail.com>
2022-03-04 19:42:58 -0500

    working with IndexedDB and service-worker

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,23 @@
+const APP_PREFIX = 'FinanceMonitor-';
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+
 const FILES_TO_CACHE = [
     "./index.html",
-    ""
-    "./assets/css/style.css",
-    "./assets/css/bootstrap.css",
-    "./assets/css/tickets.css",
-    "./dist/app.bundle.js",
-    "./dist/events.bundle.js",
-    "./dist/tickets.bundle.js",
-    "./dist/schedule.bundle.js"
-  ];
+    "./public/css/style.css",
+    // "./dist/app.bundle.js",
+    // "./dist/events.bundle.js",
+    // "./dist/tickets.bundle.js",
+    // "./dist/schedule.bundle.js"
+];
 
 self.addEventListener('install', function (e) {
-
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            console.log('installing cache : ' + CACHE_NAME)
+            return cache.addAll(FILES_TO_CACHE)
+        })
+    )
 
 
 })
